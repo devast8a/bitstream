d7c4e566a60462490bc605f8a120ffd41d5cb159
devast8a <devast8a@whitefirex.com>
2016-02-15 10:54:02 +1030

    Improve bool reading performance

--- bitstream_python/types_python_primitives.pyx
+++ bitstream_python/types_python_primitives.pyx
@@ -5,7 +5,8 @@ cdef int64 UNIT_SIZE = 8
 cdef int64 UNIT_OFFSET = UNIT_SIZE - 1
 
 cdef read_bool(BitStream stream, int64 byte, int64 bit):
-    return (stream.stream[byte] >> (UNIT_OFFSET - bit)) & 0x01 > 0
+    cdef uint8 mask = 1 << (UNIT_OFFSET - bit)
+    return (stream.stream[byte] & mask) == mask
 
 cdef write_bool(BitStream stream, value, int64 byte, int64 bit):
     if bit == 0:
